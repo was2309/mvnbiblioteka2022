d12cebea1c4d59ff4ff1a0c73eb0f72e8c92cb41
was2309 <vasilijem92@gmail.com>
2022-03-27 17:59:15 +0200

    Kreiran Maven projekat i dodate "obicne" klase

---
+++ mvnbiblioteka2022/src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2022/biblioteka/Autor.java
@@ -0,0 +1,144 @@
+package rs.ac.bg.fon.nprog.mvnbiblioteka2022.biblioteka;
+
+/**
+ * Predstavlja autora knjige.
+ * Svaki autor moze biti autor vise knjiga.
+ * @author bojantomic
+ * @version 1.0
+ *
+ */
+public class Autor {
+	
+	/**
+	 * Ime autora
+	 */
+	private String ime;
+	
+	/**
+	 * Prezime autora
+	 */
+	private String prezime;
+	
+	/**
+	 * Postavlja ime i prezime na null
+	 */
+	public Autor() {
+		ime = null;
+		prezime = null;
+	}
+
+	/**
+	 * Postavlja ime i prezime na unete vrednosti
+	 * 
+	 * @param ime novo ime autora
+	 * @param prezime novo prezime autora
+	 */
+	public Autor(String ime, String prezime) {
+		super();
+		setIme(ime);
+		setPrezime(prezime);
+	}
+
+	/**
+	 * Vraca ime autora
+	 * 
+	 * @return ime autora kao String
+	 */
+	public String getIme() {
+		return ime;
+	}
+
+	/**
+	 * Postavlja novu vrednost za atribut ime
+	 * 
+	 * @param ime novo ime autora
+	 * @throws java.lang.NullPointerException ako je uneto ime null
+	 * @throws java.lang.IllegalArgumentException ako je uneto ime prazan String
+	 */
+	public void setIme(String ime) {
+		if (ime == null)
+			throw new NullPointerException("Ime ne sme biti null");
+		
+		if (ime.isEmpty())
+			throw new IllegalArgumentException("Ime ne sme biti prazan string");
+		
+		this.ime = ime;
+	}
+
+	/**
+	 * Vraca prezime autora
+	 * 
+	 * @return prezime autora kao String
+	 */
+	public String getPrezime() {
+		return prezime;
+	}
+
+	/**
+	 * Postavlja novu vrednost za atribut prezime
+	 * 
+	 * @param prezime novo prezime autora
+	 * @throws java.lang.NullPointerException ako je uneto prezime null
+	 * @throws java.lang.IllegalArgumentException ako je uneto prezime prazan String
+	 */
+	public void setPrezime(String prezime) {
+		if (prezime == null)
+			throw new NullPointerException("Prezime ne sme biti null");
+		
+		if (prezime.isEmpty())
+			throw new IllegalArgumentException("Prezime ne sme biti prazan string");
+		
+		this.prezime = prezime;
+	}
+
+	/**
+	 * Vraca sve podatke o autoru u jednom String-u.
+	 * 
+	 * @return String sa svim podacima o autoru
+	 */
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ime == null) ? 0 : ime.hashCode());
+		result = prime * result + ((prezime == null) ? 0 : prezime.hashCode());
+		return result;
+	}
+
+	/**
+	 * Poredi dva autora po imenu i prezimenu
+	 * 
+	 * @return
+	 * <ul>
+	 * <li>true - ako su i ime i prezime isti kod oba autora</li>
+	 * <li>false - ako to nije slucaj</li>
+	 * </ul>
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (ime == null) {
+			if (other.ime != null)
+				return false;
+		} else if (!ime.equals(other.ime))
+			return false;
+		if (prezime == null) {
+			if (other.prezime != null)
+				return false;
+		} else if (!prezime.equals(other.prezime))
+			return false;
+		return true;
+	}
+
+}
---
+++ mvnbiblioteka2022/src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2022/biblioteka/Biblioteka.java
@@ -0,0 +1,89 @@
+package rs.ac.bg.fon.nprog.mvnbiblioteka2022.biblioteka;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import rs.ac.bg.fon.nprog.mvnbiblioteka2022.biblioteka.interfejs.BibliotekaInterfejs;
+
+
+
+/**
+ * Predstavlja implementaciju biblioteke sa knjigama.
+ * 
+ * Ima osnovne operacije unosa, brisanja i pretrage biblioteke.
+ */
+public class Biblioteka implements BibliotekaInterfejs {
+	
+	/**
+	 * Lista sa svim knjigama u biblioteci. Inicijalozovana kao LinkedList
+	 */
+	private List<Knjiga> knjige = new LinkedList<Knjiga>();
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new IllegalArgumentException("Ista knjiga vec postoji u biblioteci");
+		
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		if (autor == null && isbn <= 0 && naslov == null && izdavac == null)
+			throw new IllegalArgumentException("Morate uneti bar jedan kriterijum");
+		
+		List<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		for (Knjiga k : knjige) {
+			boolean nadjeno = true;
+			
+			if (naslov!=null)
+				if (k.getNaslov() != null && k.getNaslov().toLowerCase().contains(naslov.toLowerCase()))
+					nadjeno = nadjeno && true;
+				else
+					nadjeno = false;
+			
+			
+			if (isbn > 0)
+				if (k.getIsbn() == isbn) 
+					nadjeno = nadjeno && true;
+				else
+					nadjeno = false;
+			
+			
+			if (autor != null)
+				if (k.getAutori() != null && k.getAutori().contains(autor)) 
+					nadjeno = nadjeno && true;
+				else
+					nadjeno = false;
+
+			if (izdavac!=null)
+				if (k.getIzdavac() != null && k.getIzdavac().toLowerCase().contains(izdavac.toLowerCase()))
+					nadjeno = nadjeno && true;
+				else
+					nadjeno = false;
+			
+			if (nadjeno)
+				rezultat.add(k);
+		}
+		
+		return rezultat;
+	}
+
+}
